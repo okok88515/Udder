72b6a7813947f436f5cdc90641fac67f215c87ca
Weber <okok88515@gmail.com>
2023-05-24 17:49:59 +0800

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by 廖偉博 on 2023/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by 廖偉博 on 2023/5/24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
